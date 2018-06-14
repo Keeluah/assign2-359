3e950acd9d31dc5bc2ef317ce403656f07a6c39b
Kevin Ng <32828358+Keeluah@users.noreply.github.com>
2018-06-14 13:52:45 -0600

    Add files via upload

--- ball.s
+++ ball.s
@@ -4,72 +4,74 @@
 
 @ Input: r0 - snes button pressed
 updateBall:
-		push {r4-r8, lr}
+		push {r4-r9, lr}
 	ldr	r4, =ballPos
 	ldr	r5, [r4, #12] // loads ball start flag
 	
-	tst	r5, #1	// check if the ball has started moving
+	cmp	r5, #1	// check if the ball has started moving
 	beq	freeMove	
 	
-	tst	r0, #1	// check if b is pressed
+	cmp	r0, #8	// check if a is pressed
+	bne	stuckBall
 	mov	r6, #1
 	str	r6, [r4, #12]	// sets free ball flag to true
-	beq	freeMove
+	b	freeMove
 
 	//if the ball is not free, it will move with the paddle
 	
+stuckBall:
 	ldr	r7, =paddlePosition
 	ldr	r8, [r7]	// loads x position of the paddle
 	mov	r9, r8		// saves r8
-	add	r9, #32		// aligns x position to the center of the paddle
+	add	r9, #24 	// aligns x position to the center of the paddle
 	str	r9, [r4]	// stores ball position based on paddle position
-	b	exit
+	b	exitBall
 
 freeMove:
-	bl	freeMoveBall
+	bl	freeMovement
 
-exit:
-	pop {r4-r8, pc}        // leaves subroutine
+exitBall:
+	pop {r4-r9, pc}        // leaves subroutine
 
 
-.global freeMoveBall
-freeMoveBall:
-	push {r4-r8, lr}
+.global freeMovement
+freeMovement:
+	push {r4-r9, lr}
 	ldr	r4, =ballPos
 	ldr	r5, [r4] // loads x position
 	ldr	r6, [r4, #4]	// loads y position
 	ldr	r7, [r4, #8]	// loads direction of the ball
 
 testBall:
-	mov	r8, r6	// save x position
-	mov	r9, r7	// save y position
+	mov	r8, r5	// save x position
+	mov	r9, r6	// save y position
 
 testSE:	
-	tst	r7, #0	// check if direction flag is SE
+	cmp	r7, #0	// check if direction flag is SE
 	bne	testSW
-	add	r8, #1	// moves right by 1
-	add	r9, #1	// moves down by 1
+	add	r8, #2	// moves right by 1
+	add	r9, #2	// moves down by 1
 	b 	collision
 
 testSW:
-	tst	r7, #1	// check if direction flag is SW
+	cmp	r7, #1	// check if direction flag is SW
 	bne	testNE
-	sub	r8, #1 	// moves left by 1
-	add	r9, #1	// moves down by 1
+	sub	r8, #2 	// moves left by 1
+	add	r9, #2	// moves down by 1
 	b	collision
 
 testNE:
-	tst	r7, #2	// check if direction flag is NE
+	cmp	r7, #2	// check if direction flag is NE
 	bne	testNW	
-	add	r8, #1	// moves right by 1
-	sub	r9, #1	// moves up by 1
+	add	r8, #2	// moves right by 1
+	sub	r9, #2	// moves up by 1
 	b	collision		
 
 testNW:
-	tst	r7, #3	// check if direction flag is NW
+	cmp	r7, #3	// check if direction flag is NW
 	bne	moveExit	// exits cause error in the direction flag
-	sub	r8, #1	// moves left by 1
-	sub	r9, #1	// moves up by 1
+	sub	r8, #2	// moves left by 1
+	sub	r9, #2	// moves up by 1
 
 collision:
 	mov	r0, r8	// passes new x position
@@ -87,15 +89,16 @@ savePosition:
 	str	r9, [r4, #4]	// saves y position
 	
 moveExit:
-	pop {r4-r8, pc}        // leaves subroutine
+	pop {r4-r9, pc}        // leaves subroutine
 
 .global collisionCheck
 @ input: r0 - x position, r1 - y position
 @ output: r0 - collision flag (1 for collided)
 @	  r1 - ball direction (if changed)
 collisionCheck:
+	push {r4-r8, lr}
 	mov	r0, #0
-	mov	pc, lr
+	pop {r4-r8, pc}
 
 @ Data section
 .section .data
