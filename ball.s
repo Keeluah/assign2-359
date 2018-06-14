c5ebe81814a07a69b18a873a4cefaa37a3bdf9d7
Kevin Ng <32828358+Keeluah@users.noreply.github.com>
2018-06-14 15:11:33 -0600

    Add files via upload

--- ball.s
+++ ball.s
@@ -49,39 +49,41 @@ testBall:
 testSE:	
 	cmp	r7, #0	// check if direction flag is SE
 	bne	testSW
-	add	r8, #2	// moves right by 1
-	add	r9, #2	// moves down by 1
+	add	r8, #3	// moves right by 1
+	add	r9, #3	// moves down by 1
 	b 	collision
 
 testSW:
 	cmp	r7, #1	// check if direction flag is SW
 	bne	testNE
-	sub	r8, #2 	// moves left by 1
-	add	r9, #2	// moves down by 1
+	sub	r8, #3 	// moves left by 1
+	add	r9, #3	// moves down by 1
 	b	collision
 
 testNE:
 	cmp	r7, #2	// check if direction flag is NE
 	bne	testNW	
-	add	r8, #2	// moves right by 1
-	sub	r9, #2	// moves up by 1
+	add	r8, #3	// moves right by 1
+	sub	r9, #3	// moves up by 1
 	b	collision		
 
 testNW:
 	cmp	r7, #3	// check if direction flag is NW
 	bne	moveExit	// exits cause error in the direction flag
-	sub	r8, #2	// moves left by 1
-	sub	r9, #2	// moves up by 1
+	sub	r8, #3	// moves left by 1
+	sub	r9, #3	// moves up by 1
 
 collision:
 	mov	r0, r8	// passes new x position
 	mov	r1, r9	// passes new y position
+	mov	r2, r7	// passes on the ball direction
 	bl	collisionCheck	// checks if new position collides with something
-
-@returns collision flag in r0 and new direction in r1
-	tst	r0, #0	// check if the flag is off
+	cmp	r3, #1
+	beq	moveExit
+	cmp	r0, #0	// check if the flag is off
 	beq	savePosition	// goes to save if it did not collide
 	str	r1, [r4, #8]	// stores new direction
+	mov	r7, r1	// change direction for the reloop
 	b	testBall	// loops again with new direction
 
 savePosition:
@@ -92,12 +94,71 @@ moveExit:
 	pop {r4-r9, pc}        // leaves subroutine
 
 .global collisionCheck
-@ input: r0 - x position, r1 - y position
+@ input: r0 - x position, r1 - y position, r2 - direction
 @ output: r0 - collision flag (1 for collided)
 @	  r1 - ball direction (if changed)
 collisionCheck:
 	push {r4-r8, lr}
+
+	mov	r4, r0	// saves x position
+	mov	r5, r1	// saves y position
+	mov	r6, r2	// saves direction
+
+collideLeftWall:
+	cmp	r4, #114
+	bgt	collideRightWall
+	cmp	r6, #1	// check if ball moving SW
+	moveq	r6, #0	// change to SE if so
+	cmp	r6, #3	// check if ball moving NW
+	moveq	r6, #2	// change to NE if so
+	b 	collided	// tells routine ball collided returns to calling
+
+collideRightWall:
+	ldr	r7, =screenRestrict
+	ldr	r8, [r7, #4]
+	add	r8, #48
+	cmp	r4, r8
+	blt	collideUpWall
+	cmp	r6, #0	// check if ball moving SE
+	moveq	r6, #1	// change to SW if so
+	cmp	r6, #2	// check if ball moving NE
+	moveq	r6, #3	// change to NW if so
+	b 	collided	// tells routine ball collided returns to calling
+
+collideUpWall:
+	cmp	r5, #79
+	bgt	collideBottom
+	cmp	r6, #2	// check if ball moving NE
+	moveq	r6, #0	// change to SE if so
+	cmp	r6, #3	// check if ball moving NW
+	moveq	r6, #1	// change to SW if so
+	b 	collided
+
+collideBottom:
+	ldr	r7, =bottomScreen
+	ldr	r8, [r7]
+	cmp	r5, r8	// check if the ball hit the bottom of the screen
+	blt	collideBrick	// continues collision check otherwise
+	ldr	r7, =ballPos	// loads address of ball position
+	mov	r8, #0
+	str	r8, [r7, #12]
+	mov	r8, #2
+	str	r8, [r7, #8]
+	mov	r8, r5	
+	sub	r8, #64
+	str	r8, [r7, #4]
+	mov	r3, #1
+	b	exitCheck
+
+collideBrick:
 	mov	r0, #0
+	b	exitCheck
+
+collided:
+	mov	r0, #1
+
+exitCheck:
+	mov	r1, r6
 	pop {r4-r8, pc}
 
 @ Data section
@@ -108,3 +169,6 @@ ballPos:
 	.int	674	// y pos
 	.int	2	// direction (0 - SE, 1 - SW, 2 - NE, 3 - NW)
 	.int	0	// flags if the ball has left the paddle
+
+bottomScreen:
+	.int 740
