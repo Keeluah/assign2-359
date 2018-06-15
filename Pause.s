de170857ecb58aa0ef934082d8c11f36e39e8625
dmxtrix <25750968+dmxtrix@users.noreply.github.com>
2018-06-14 19:31:05 -0600

    Add files via upload

--- Pause.s
+++ Pause.s
@@ -3,11 +3,11 @@
 .global Pause
 .global Restart
 Pause:
-	push	{r4, lr}
+	push	{r4-r9, lr}
 	bl	snesRead
 	teq	r0, #2		// check if select is pressed
 	beq	Stop		// stop the game if select is pressed
-	pop	{r4, pc}	// continue the game	
+	pop	{r4-r9, pc}	// continue the game	
 
 Stop:
 	mov	r4, #200
@@ -29,18 +29,35 @@ Stop:
 	add	r4, #74
 	str	r4, [r5, #4]		// resets the ball y position 
 
-	ldr	r4, =mapData
-	ldr	r5, =backUpMap
-	str	r5, [r4]
+	ldr	r5, =mapData
+	ldr	r6, =backUpMap
+	mov	r7, #0 	
+	bl	pauseFor
 
-	bl 	drawHome		// goes back to the main menu
+pauseFor:
+	ldr	r4, =mapSize
+	ldr	r8, [r4]
+		
+
+	ldrb	r9, [r6, r7]
+	strb	r9, [r5, r7]
+
+	add	r7, #1
+	cmp	r7, r8
+	bne	pauseFor
+
+	mov	r0, #1
+	pop	{r4-r9, pc}		// for loop is for setting all of the bricks back to the original position
+
+
+
 
 Restart:
-	push	{r4, lr}
+	push	{r4-r9, lr}
 	bl	snesRead
 	teq	r0, #3		// check if select is pressed
 	beq	loop		// stop the game if select is pressed
-	pop	{r4, pc}	// continue the game
+	pop	{r4-r9, pc}	// continue the game
 
 loop:
 	mov	r4, #200
@@ -62,14 +79,39 @@ loop:
 	add	r4, #74
 	str	r4, [r5, #4]		// resets the ball y position 
 
-	ldr	r4, =mapData
-	ldr	r5, =backUpMap
-	str	r5, [r4]
+	ldr	r5, =mapData
+	ldr	r6, =backUpMap
+	mov	r7, #0 	
+	bl	restartFor
 
-	bl 	mainLoop		// restarts the game 
+	mov	r0, #1
+	pop	{r4-r9, pc}
+
+
+
+restartFor:				// for loop is for setting all of the bricks back to the original position
+	ldr	r4, =mapSize		
+	ldr	r8, [r4]
+		
+
+	ldrb	r9, [r6, r7]
+	strb	r9, [r5, r7]
+
+	add	r7, #1
+	cmp	r7, r8
+	bne	pauseFor
+
+	mov	r0, #1
+	pop	{r4-r9, pc}
+
+
+	mov	r0, #1
+	pop	{r4-r9, pc}
 
 
 		
-
+.global mapSize
+mapSize:
+	.int	264
 
 	
